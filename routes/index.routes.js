cbc7744492c40842422ab5e87ab2217cdf732de5
damian1997 <d.veltkamp137@gmail.com>
2021-02-18 16:44:47 +0100

    PT-1: Oopsie

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/', base.home)
 router.get('/register', account.register)
-router.post('/register-user', account.registerUser, (req, res) => {
+router.post('/register', account.registerUser, (req, res) => {
   if(Object.keys(req.errors).length && req.errors.constructor === Object) {
     return account.register(req, res)
   } else {
