faeacae80d1df31c920cef63a4fc363e85beffff
damian1997 <d.veltkamp137@gmail.com>
2021-03-06 13:12:11 +0100

    PT-1: Refactored clientside validation script

--- source/components/form/javascript/form.js
+++ source/components/form/javascript/form.js
@@ -1,27 +1,54 @@
-export function initLoginHandler() {
-  document.querySelector('[js-hook-module-login]').addEventListener('submit', loginHandler)
-}
-
-export function initRegisterHandler() {
-  console.log('heeyo')
+function initFormValidation() {
+  document.querySelector('[js-hook-module-form-validation]').addEventListener('submit', loginHandler)
 }
 
 function loginHandler(event) {
-  document.querySelectorAll('[js-hook-validate]').forEach(node => {
+  event.preventDefault()
+
+  let errors = false
+
+  const validateNodes = document.querySelectorAll('[js-hook-validate]')
+
+  validateNodes.forEach(node => {
+    console.log(node)
     switch(node.name) {
       case 'email' : {
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         const valid = regex.test(node.value)
 
-        valid == false ? appendError(node, 'Provided email is not a valid email adress') : event.target.submit()
+        if(valid === false) {
+          appendError(node, 'Provided email is not a valid email adress')
+        } else {
+          removeError(node)
+        }
+
         break;
       }
       case 'password' : {
         const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/
         const valid = regex.test(node.value)
-        console.log(valid)
 
-        valid == false ? appendError(node, 'Password does not match our patern criteria') : event.target.submit()
+        if(valid == false) {
+          appendError(node, 'Password does not match our patern criteria')
+          errors = true
+        } else {
+          removeError(node)
+        }
+
+        break;
+      }
+      case 'repeatPassword' : {
+        const password = [...validateNodes].find(node => {
+          return node.name === 'password'
+        })
+
+        if(node.value !== password.value) {
+          appendError(node, 'Passwords are not the same')
+          errors = true
+        } else {
+          removeError(node)
+        }
+
         break;
       }
       default: {
@@ -29,17 +56,41 @@ function loginHandler(event) {
       }
     }
   })
-  event.preventDefault()
+
+  errors === false && event.target.submit()
 }
 
 function appendError(inputNode, errorMessage) {
-  const errorNode = document.createElement('span')
-  const textNode = document.createTextNode(errorMessage)
-  errorNode.appendChild(textNode)
+  const errorNodeExists = getErrorContainer(inputNode)
 
-  errorNode.classList.add('form__error')
+  if(errorNodeExists !== null) {
+    errorNodeExists.innerHTML = errorMessage
+    return
+  }
 
+  const errorNode = createErrorNode(errorMessage)
   inputNode.parentNode.insertBefore(errorNode, inputNode)
 }
 
-export default initLoginHandler
+function getErrorContainer(inputNode) {
+  const previousSibling = inputNode.previousElementSibling
+
+  return previousSibling.classList.contains('form__error') === true ? previousSibling : null
+}
+
+function createErrorNode(errorMessage) {
+  const errorNode = document.createElement('span')
+  const textNode = document.createTextNode(errorMessage)
+  errorNode.classList.add('form__error')
+
+  errorNode.appendChild(textNode)
+  return errorNode
+}
+
+function removeError(inputNode) {
+  const errorNodeExists = getErrorContainer(inputNode)
+
+  errorNodeExists !== null && errorNodeExists.remove()
+}
+
+export default initFormValidation
--- source/main.js
+++ source/main.js
@@ -1,10 +1,5 @@
 import moduleInit from '../source/javascript/utilities/module-init'
 
-moduleInit('[js-hook-module-login]', () => {
+moduleInit('[js-hook-module-form-validation]', () => {
   return import('../source/components/form/javascript/form')
-}, 'initLoginHandler')
-
-moduleInit('[js-hook-module-register]', () => {
-  return import('../source/components/form/javascript/form')
-}, 'initRegisterHandler')
-
+})
